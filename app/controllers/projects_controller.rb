834e49fa99a23c2b1ec534c56c3d1ba20241ea87
Robert Cardona <rfcardona@gmail.com>
2015-08-12 13:46:38 -0400

    initial

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,74 @@
+class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
+  # GET /projects
+  # GET /projects.json
+  def index
+    @projects = Project.all
+  end
+
+  # GET /projects/1
+  # GET /projects/1.json
+  def show
+  end
+
+  # GET /projects/new
+  def new
+    @project = Project.new
+  end
+
+  # GET /projects/1/edit
+  def edit
+  end
+
+  # POST /projects
+  # POST /projects.json
+  def create
+    @project = Project.new(project_params)
+
+    respond_to do |format|
+      if @project.save
+        format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        format.json { render :show, status: :created, location: @project }
+      else
+        format.html { render :new }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /projects/1
+  # PATCH/PUT /projects/1.json
+  def update
+    respond_to do |format|
+      if @project.update(project_params)
+        format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+        format.json { render :show, status: :ok, location: @project }
+      else
+        format.html { render :edit }
+        format.json { render json: @project.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /projects/1
+  # DELETE /projects/1.json
+  def destroy
+    @project.destroy
+    respond_to do |format|
+      format.html { redirect_to projects_url, notice: 'Project was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_project
+      @project = Project.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def project_params
+      params.require(:project).permit(:title, :description, :date, :url1, :url2, :url3, :url4, :kitchen, :sqft, :number_of_cabinets, :countertop_style, :counter_top_length, :number_of_outlets, :light_fixture_type, :floor_sqft, :floor_type, :bath, :vanity_width, :tub_length, :shower_sqft, :mirror, :bath_fan, :air_conditioning, :ac_type, :home_sqft, :ac_size, :duct_work, :roof, :exterior_general, :more_detail, :paint, :floor, :floor_demo, :location_city, :budget, :ends, :pool, :drive_way, :length, :width, :landscaping, :fence, :fence_material, :windows, :window_type, :quantity, :electrical, :plumbing, :handy_man)
+    end
+end
---
+++ db/schema.rb
@@ -0,0 +1,124 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20150812024624) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "date"
+    t.string   "url1"
+    t.string   "url2"
+    t.string   "url3"
+    t.string   "url4"
+    t.boolean  "kitchen"
+    t.string   "sqft"
+    t.integer  "number_of_cabinets"
+    t.string   "countertop_style"
+    t.integer  "counter_top_length"
+    t.integer  "number_of_outlets"
+    t.string   "light_fixture_type"
+    t.integer  "floor_sqft"
+    t.string   "floor_type"
+    t.boolean  "bath"
+    t.integer  "vanity_width"
+    t.integer  "tub_length"
+    t.integer  "shower_sqft"
+    t.string   "mirror"
+    t.string   "bath_fan"
+    t.boolean  "air_conditioning"
+    t.string   "ac_type"
+    t.integer  "home_sqft"
+    t.integer  "ac_size"
+    t.string   "duct_work"
+    t.boolean  "roof"
+    t.boolean  "exterior_general"
+    t.text     "more_detail"
+    t.boolean  "paint"
+    t.boolean  "floor"
+    t.string   "floor_demo"
+    t.string   "location_city"
+    t.integer  "budget"
+    t.string   "ends"
+    t.boolean  "pool"
+    t.boolean  "drive_way"
+    t.integer  "length"
+    t.integer  "width"
+    t.boolean  "landscaping"
+    t.boolean  "fence"
+    t.string   "fence_material"
+    t.boolean  "windows"
+    t.string   "window_type"
+    t.integer  "quantity"
+    t.boolean  "electrical"
+    t.boolean  "plumbing"
+    t.boolean  "handy_man"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "vendors", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "vendors", ["email"], name: "index_vendors_on_email", unique: true
+  add_index "vendors", ["reset_password_token"], name: "index_vendors_on_reset_password_token", unique: true
+
+end
